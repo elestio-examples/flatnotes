defd5ad62392a2a58c284aa5c3429e07e7e98da0
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-06-15 11:47:51 +0300

    fix

--- tests/build.sh
+++ tests/build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+sed -i "s~FROM --platform=\$BUILDPLATFORM python:3.11-slim-bullseye AS build~FROM python:3.11-slim-bullseye AS build~g" ./Dockerfile
 
 docker build . --tag elestio4test/flatnotes:latest
